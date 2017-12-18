e4dc8cf26c25b85bc89dc8c044799c0b2e4a525e
hiroki-hoshida <hiroki.lincoln@gmail.com>
2017-12-17 22:03:23 -0500

    First Mockup

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Hiroki on 12/17/2017.
+ */
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+});
+
+$(document).ready(function() {
+    $('select').material_select();
+});
