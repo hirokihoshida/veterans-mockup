886ef34338ea5f5d2c3f002962b6c057ac272a43
hirokihoshida <hiroki.lincoln@gmail.com>
2017-12-19 10:45:13 -0500

    Updated colors, modularized

---
+++ head.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Mainpage</title>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <style type="text/css">
+
+        body {
+            background-color: #eceff1
+        }
+    </style>
+</head>
--- js/index.js
+++ js/index.js
@@ -8,3 +8,4 @@ $(document).ready(function(){
 $(document).ready(function() {
     $('select').material_select();
 });
+
