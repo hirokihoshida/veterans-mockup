f4c2ec1a5d5929be6257bc8fae24107e00064cc5
hirokihoshida <hiroki.lincoln@gmail.com>
2018-01-18 09:30:15 -0500

    added homepage

---
+++ home.php
@@ -0,0 +1,48 @@
+<?php include 'head.php'; ?>
+
+<body>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+<script type="text/javascript" src="js/index.js"></script>
+
+<?php include 'navbar.php'; ?>
+
+<div class="container">
+    <div class="section">
+        <div class="row">
+            <br>
+            <br>
+            <div class="col s6">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="Clients.jpg">
+                        <span class="card-title"><b>Clients</b></span>
+                    </div>
+                    <div class="card-content">
+                        <p>Information and data for all Bergen County Veterans Services clients.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#" class="black-text">View</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s6">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="Report.jpg">
+                        <span class="card-title black-text"><b>Reports</b></span>
+                    </div>
+                    <div class="card-content">
+                        <p>Generate reports for individual client or entire database.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#" class="black-text">Individual</a>
+                        <a href="#" class="black-text">Group</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
