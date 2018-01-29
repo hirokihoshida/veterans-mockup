41dbbf60e71ebd05e64bb8c5182cc58bb0360735
hirokihoshida <hiroki.lincoln@gmail.com>
2018-01-29 08:50:36 -0500

    New homepage

--- head.blade.php
+++ head.blade.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="UTF-8">
-    <title>Veteran Services</title>
+    <title>Veteran Services1</title>
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
@@ -12,6 +12,7 @@
 
         body {
             background-color: #eceff1
+            
         }
     </style>
 </head>
