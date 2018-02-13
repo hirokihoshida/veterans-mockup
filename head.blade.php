f4753c2a1f8f6e39b1973d1946f63de820e27184
hirokihoshida <hiroki.lincoln@gmail.com>
2018-02-13 08:37:16 -0500

    Visits Picture

--- head.blade.php
+++ head.blade.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="UTF-8">
-    <title>Veteran Services1</title>
+    <title>Veteran Services</title>
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
